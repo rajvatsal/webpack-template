09751ca3e6ee14b914cf303b5d85b03de32e1757
Vatsal <rajvatsal2004@gmail.com>
2024-06-09 03:46:17 +0530

    feat(webpack-pro): Install babel

--- webpack.pro.js
+++ webpack.pro.js
@@ -3,4 +3,16 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
 	mode: "production",
+	rules: [
+		{
+			test: /\.(?:js|mjs|cjs)$/,
+			exclude: /node_modules/,
+			use: {
+				loader: "babel-loader",
+				options: {
+					presets: [["@babel/preset-env", { targets: "defaults" }]],
+				},
+			},
+		},
+	],
 });
